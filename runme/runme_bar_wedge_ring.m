c93a0f16093d6d790701c10d77f0458c4e521b8f
test <winawerlab@STIMULUS-MAC.CBI.FAS.NYU.EDU>
2022-11-12 15:41:33 -0500

    adding runme file for bar_wedge+ring (NSD design).

---
+++ runme/runme_bar_wedge_ring.m
@@ -0,0 +1,32 @@
+tbUse vistadisp
+% runme for bar wedge ring
+% 300TRs
+
+%% 
+ 
+params = retCreateDefaultGUIParams;
+params.fixation = 'dot with grid';
+params.tr = 1;
+params.skipSyncTests = 0;
+params.calibration = 'CBI_Propixx';
+params.prescanDuration = 0;
+params.experiment  = 'experiment from file';
+params.doEyelink = false;
+params.period = 300;
+params.responseDevice      = '932';
+params.keyboard = 'Magic Keyboard';
+params.responseKeys        = {'1';'2';'3';'4';'6';'7';'8';'9'};
+
+%% stim file
+
+%% run it
+explist = {'bar_300TRs_5hz_1';'wedgering_300TRs_5hz_1';'bar_300TRs_10hz_1';'wedgering_300TRs_10hz_1';'bar_300TRs_15hz_1';'wedgering_300TRs_15hz_1'};
+
+for ii = 2:length(explist)
+    params.loadMatrix = sprintf('%s.mat', explist{ii});
+    ret(params);
+end
+
+
+% Edit in dispStringInCenter
+% Screen(?Preference?,?TextRenderer?, 0);
