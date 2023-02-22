7271a286a9e73dc1f064bd16adfb3fbb2b99c361
winawerlab <winawerlab@STIMULUS-MAC.CBI.FAS.NYU.EDU>
2023-02-21 23:49:10 -0500

    addding keyboard and button box settings to old runme

--- runme/runme_colorfulStim.m
+++ runme/runme_colorfulStim.m
@@ -11,16 +11,18 @@ params.skipSyncTests        = 0;
 params.calibration          = 'CBI_Propixx';
 params.prescanDuration      = 0;
 params.experiment           = 'experiment from file';
-params.doEyelink            = false;
+params.doEyelink            = true;
 params.period               = 192;
 params.responseDevice       = '932';
 params.keyboard             = 'Magic Keyboard';
 params.responseKeys         = {'1';'2';'3';'4';'6';'7';'8';'9'};
+params.displayGUI           = false;
 %% stim file
 
 %% run it
 
 for ii = 1:6
+    params.sesNum = ii;
     params.loadMatrix = sprintf('ret_%d.mat', ii);
     ret(params);
 end
